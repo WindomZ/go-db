fdcf6c2d64caefdd07c0e91a41d0dc059e0bbfe0
WindomZ <zwdsdtc@gmail.com>
2016-07-21 20:56:40 +0800

    feat: pgsql tx mutex

--- pgdb/pgsql/tx.go
+++ pgdb/pgsql/tx.go
@@ -4,16 +4,19 @@ import "github.com/jmoiron/sqlx"
 
 type Tx struct {
 	sqlx.Tx
+	db        *DB
 	isolation bool
 }
 
 func (db *DB) BeginX(tx *sqlx.Tx) (*Tx, error) {
 	if tx == nil {
+		db.txMutex.Lock()
 		var err error
 		if tx, err = db.Beginx(); err != nil {
+			db.txMutex.Unlock()
 			return nil, err
 		}
-		return &Tx{Tx: *tx, isolation: false}, nil
+		return &Tx{Tx: *tx, db: db, isolation: false}, nil
 	}
 	return &Tx{Tx: *tx, isolation: true}, nil
 }
@@ -25,6 +28,8 @@ func (tx *Tx) GetTx() *sqlx.Tx {
 func (tx *Tx) Rollback() error {
 	if tx.isolation {
 		return nil
+	} else if tx.db != nil {
+		tx.db.txMutex.Unlock()
 	}
 	return tx.Tx.Rollback()
 }
@@ -32,6 +37,8 @@ func (tx *Tx) Rollback() error {
 func (tx *Tx) Commit() error {
 	if tx.isolation {
 		return nil
+	} else if tx.db != nil {
+		tx.db.txMutex.Unlock()
 	}
 	return tx.Tx.Commit()
 }
